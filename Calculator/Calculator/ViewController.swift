29ce218feb8386b131c68829bd74818a25ef3301
Gabor L Lizik <gabor.lizik@sanoma.com>
2015-02-01 10:51:36 +0100

    Init, open new project.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gabor L Lizik on 01/02/15.
+//  Copyright (c) 2015 Gabor L Lizik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
