c8bd1e7fff46e8c0896766b1b0cb1e63afad4528
Gabor L Lizik <gabor.lizik@sanoma.com>
2015-02-10 16:44:43 +0100

    2. More Xcode and Swift, MVC

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
         case "÷": performOperation { $1 / $0 }
         case "+": performOperation { $0 + $1 }
         case "−": performOperation { $1 - $0 }
+        case "√": performOperation {sqrt($0)}
         default: break
     }
 }
@@ -47,6 +48,13 @@ class ViewController: UIViewController {
         }
     }
     
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
         operandStack.append(displayValue)
